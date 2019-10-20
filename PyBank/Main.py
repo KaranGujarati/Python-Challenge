490e4984e011d74471d4d2b30f1bdb17853a1282
Karan Gujarati <karan28gujarati@gmail.com>
2019-10-20 12:52:13 -0400

    CleanCode
    One error in Pybank-couldn't rectify it. will need your help to fix it

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,19 +1,21 @@
 import os
 import csv
 
-file_to_load = os.path.join("Resources", "budget_data.csv")
-file_to_output = os.path.join("Output", "budget_summary.txt")
+filepath = os.path.join("Resources", "budget_data.csv")
+output_file = os.path.join("Output", "budget_summary.txt")
 
-#total_months = []
-#total_amount = []
-#total_amount = list(map(int, total_amount))
 
 total_months = 0
 total_amount = 0
 
+previous_rev = 0
+revenue_diff = 0
+greatest_incr = ["", 0]
+greatest_decr = ["", 9999999999999999999999]
+
 revenue_changes = []
 
-with open(file_to_load) as bgt_data:
+with open(filepath) as bgt_data:
     csvreader = csv.DictReader(bgt_data)
 
 
@@ -22,35 +24,41 @@ with open(file_to_load) as bgt_data:
         total_months = total_months + 1
         total_amount = total_amount + int(row["Profit/Losses"])
 
+
+
+        revenue_diff = int(row["Profit/Losses"]) - previous_rev
+        previous_rev = int(row["Profit/Losses"])
+        #print (revenue_diff) 
+        
+        if (revenue_diff > greatest_incr[1]):
+            greatest_incr = revenue_diff
+            greatest_incr[0] = row["Date"]
+
+        if (revenue_diff < greatest_decr[1]):
+            greatest_decr[1] = revenue_diff
+            greatest_decr[0] = row["Date"]
+
         revenue_changes.append(int(row["Profit/Losses"]))
         
     avg_changes= sum(revenue_changes) / len(revenue_changes)
-    #avg_changes= total_amount / total_months
-
+    
 
     print("Total Months: " + str(total_months))
     print("Net Total amount: " + str(total_amount))
-    print("Average Change: " + "$" + str(round(sum(revenue_changes) / len(revenue_changes),2)))
+    print("Average Change: " + "$" + str(round(sum(revenue_diff) / len(revenue_diff),2)))
+    print("Greatest Increase: " + " ($" +  str(greatest_incr[1]) + ")") 
+    print("Greatest Decrease: " + " ($" +  str(greatest_decr[1]) + ")")
 
         
-  #      total_months = total_months + [row[0]]
 
- #       total_amount = total_amount + [row[1]]
-        
-
-#print ("The total number of months : " + str(len(total_months)))
-
-#print ("The net total amount over the entire period : ", str(total_amount))
-#print (total_amount)
 
 # Output Files
-with open(file_to_output, "w") as txt_file:
+with open(output_file, "w") as txt_file:
     txt_file.write("Total Months: " + str(total_months))
     txt_file.write("\n")
-    txt_file.write("Total Revenue: " + "$" + str(total_revenue))
+    txt_file.write("Total Revenue: " + "$" + str(total_amount))
     txt_file.write("\n")
-    txt_file.write("Average Change: " + "$" + str(round(sum(revenue_changes) / len(revenue_changes),2)))
-
+    txt_file.write("Average Change: " + "$" + str(round(sum(revenue_diff) / len(revenue_diff),2)))
 
 
 
